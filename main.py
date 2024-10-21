b8a8e54f38cc994a2dc3e6068bc6fd70dc79db93
Irina16111982 <i.a.kaparushkina@tmn3.etagi.com>
2024-10-21 08:31:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+name="Ирина"  #Имя
+print(name,type(name))
+age=41        #Возраст
+print(age,type(age))
+age=age+1     #новое числовое значение
+print(age,type(age))
+is_student=True # студент - верно, как оказалось,True важно писать с заглавной буквы, а не со строчной.
+print(is_student,type(is_student))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
